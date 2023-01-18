b4b20f8134167ffdd5d571aada83de6d70299eaa
Yuma Satake <satous.y@icloud.com>
2023-01-18 21:24:10 +0900

    フォントの追加

--- front/nkc-cloud/src/App.tsx
+++ front/nkc-cloud/src/App.tsx
@@ -5,7 +5,6 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-
 const mainTheme = createTheme({
   typography: {
     fontFamily:[
