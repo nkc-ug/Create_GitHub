96f9674d49e24e8eb3fc2b89dd87134be5abb771
Yuma Satake <satous.y@icloud.com>
2023-01-18 14:29:44 +0900

    ディレクトリ変更

--- front/nkc-cloud/src/util/axiosUtil.ts
+++ front/nkc-cloud/src/util/axiosUtil.ts
@@ -3,7 +3,7 @@ import {Axios} from "axios";
 const axios = new Axios({
     baseURL: "http://localhost:3003",
     responseType:"json",
-    transformResponse: [
+    transformResponse:[
         function transformResponse(data){
             return JSON.parse(data);
         }
