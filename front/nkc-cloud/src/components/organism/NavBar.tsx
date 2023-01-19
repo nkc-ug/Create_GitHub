240b7d62a7122777807b0e421c7ec81a0c5c224f
Yuma Satake <satous.y@icloud.com>
2023-01-19 11:20:05 +0900

    drawer追加

--- front/nkc-cloud/src/components/organism/NavBar.tsx
+++ front/nkc-cloud/src/components/organism/NavBar.tsx
@@ -1,13 +1,14 @@
-import { AppBar, Button, Container, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Container, Drawer, IconButton, Modal, Toolbar, Typography } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import { useNavigate } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-
+import { useState } from "react";
 import logo from "../../img/logo.png";
 
 const NavBar:React.FC = () => {
     const navigation = useNavigate();
+    const [ModalOpen, setModalOpen] = useState<boolean>(false);
 
     const chengeUser = () => {
         navigation("/")
@@ -60,15 +61,42 @@ const NavBar:React.FC = () => {
             {/* forPhone */}
             <Container disableGutters>
                 <Toolbar sx={{ display:{ xs:'flex', md: 'none' } }}>
-                    <IconButton size="large">
+                    <IconButton onClick={() => {setModalOpen(!ModalOpen)}} size="large">
                         <MenuIcon fontSize="large"/>
                     </IconButton>
-                    <Button sx={{ my:1, mx:'auto' }}>
+                    <Button onClick={chengeHome} sx={{ my:1, mx:'auto' }}>
                         <img src={logo} alt="logo" width={100}/>
                     </Button>
-                    <MenuIcon fontSize="large" sx={{ color:"secondary.main" }}/>
+                    {/* 以下ボタン位置調整用 */}
+                    <IconButton size="large" disabled>
+                        <MenuIcon fontSize="large" sx={{ color:"secondary.main" }}/>
+                    </IconButton>
                 </Toolbar>
             </Container>
+            <Drawer
+                anchor='top'
+                open={ModalOpen}
+                onClose={() => {setModalOpen(!ModalOpen)}}
+                sx={{color:'secodary.main'}}
+            >
+                <Stack direction='column' alignContent='center' sx={{ bgcolor:'secondary.main', py:2 }}>
+                    {AppBarItemList.map(( AppBarItem ) => {
+                        return(
+                            <Button key={AppBarItem.text} onClick={AppBarItem.action} sx={{ mb:1 }}>
+                                <Typography variant="h6">
+                                    {AppBarItem.text}
+                                </Typography>
+                            </Button>
+                        )
+                    })}
+                    <Button onClick={chengeUser}>
+                        <AccountCircleIcon fontSize="large" sx={{ color:'primary.main', mr:1 }}/>
+                        <Typography variant="h6">
+                            ユーザ設定
+                        </Typography>
+                    </Button>
+                </Stack>
+            </Drawer>
         </AppBar>
     )
 }
