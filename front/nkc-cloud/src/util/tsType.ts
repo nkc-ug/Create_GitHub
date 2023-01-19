82dd6eac06e2506737d4369ac074ffe3088fc071
Yuma Satake <satous.y@icloud.com>
2023-01-19 11:20:29 +0900

    type追加

--- front/nkc-cloud/src/util/tsType.ts
+++ front/nkc-cloud/src/util/tsType.ts
@@ -1,3 +1,14 @@
-export type TTsxChild = {
+// import { SvgIconTypeMap } from "@mui/material"
+// import { OverridableComponent } from "@mui/material/OverridableComponent"
+
+export type TsxChild = {
     children: React.ReactNode
 }
+
+// 後で修正して適用する
+// export type SecondaryButton = {
+//     state: boolean,
+//     stateAction: React.SetStateAction<boolean>,
+//     icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">>,
+//     label: String,
+// }
