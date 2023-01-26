ed4ac9427f4aca6789d988ad48b5603fa917d6ca
Yuma Satake <satous.y@icloud.com>
2023-01-26 20:08:12 +0900

    sendPageへのルーティング追加

--- front/nkc-cloud/src/router/pageRouter.tsx
+++ front/nkc-cloud/src/router/pageRouter.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ErrorPage from "../components/pages/ErrorPage";
 import HomePage from "../components/pages/HomePage";
+import SendPage from "../components/pages/SendPage";
 
 const PageRouter:React.FC = () => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<HomePage/>}/>
+                <Route path={"/SendPage"} element={<SendPage/>}/>
                 <Route path={"/*"} element={<ErrorPage/>}/>
             </Routes>
         </BrowserRouter>
