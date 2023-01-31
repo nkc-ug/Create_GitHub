2d09f1c5fa72cd7a17c7536148632fb2f44ea565
Yuma Satake <satous.y@icloud.com>
2023-01-31 12:42:05 +0900

    send・downloadページ作成

--- front/nkc-cloud/src/components/pages/SendPage.tsx
+++ front/nkc-cloud/src/components/pages/SendPage.tsx
@@ -1,35 +1,27 @@
 import { TextField, Typography } from "@mui/material"
 import { Box, Stack } from "@mui/system"
 import { useContext } from "react";
-import { FileAboutCommentContext, FileAboutFileNameContext, FileAboutKeyContext, FileAboutTitleContext, FileAboutUrlContext } from "../../App";
-import MainLayout from "../template/MainLayout"
+import { FileAboutContext } from "../../App";
+import TopLayout from "../template/BaseLayout";
+import InfoLayout from "../template/InfoLayout";
 
 const SendPage:React.FC = () => {
-    const {state:FileAboutTitle, setState:setFileAboutTitle} = useContext(FileAboutTitleContext);
-    const {state:FileAboutComment, setState:setFileAboutComment} = useContext(FileAboutCommentContext);
-    const {state:FileAboutFileName, setState:setFileAboutFileName} = useContext(FileAboutFileNameContext);
-    const {state:FileAboutKey, setState:setFileAboutKey} = useContext(FileAboutKeyContext);
-    const {state:FileAboutUrl, setState:setFileAboutUrl} = useContext(FileAboutUrlContext);
-
+    const {state:FileAbout} = useContext(FileAboutContext);    
 
     return(
-        <MainLayout>
-            <Stack justifyContent='center' direction='row' sx={{ mt:5 }}>
-                <Box sx={{ mx:3, p:3, borderRadius:2,  bgcolor:'secondary.main', boxShadow:1 }}>
-                    <Typography textAlign='center' variant='h6' noWrap={true} sx={{ mb:3 }}>
-                        ファイルの送信が完了しました！
-                    </Typography>
-                    <Stack direction='column' spacing={4} justifyContent='center' alignContent='center' sx={{ m:2 }}>
-                        <Typography variant="body1">タイトル　：　{FileAboutTitle === "" ? "タイトル未設定" : FileAboutTitle }</Typography>
-                        <Typography variant="body1">コメント　：　{FileAboutComment === "" ? "コメント未設定" : FileAboutComment }</Typography>
-                        <Typography variant="body1">ファイル名：　{FileAboutFileName === "" ? "" : FileAboutFileName }</Typography>
-                        <Typography variant="body1">パスワード：　{FileAboutKey === "0000" ? "パスワードが設定されていません" : FileAboutKey }</Typography>
-                        <TextField value={FileAboutUrl} variant="standard"/>
-                    </Stack>
-                </Box>
+        <InfoLayout>
+            <Typography textAlign='center' variant='h5' noWrap={true} sx={{ mb:3 }}>
+                ファイルの送信が完了しました！
+            </Typography>
+            <Stack direction='column' spacing={4} justifyContent='center' alignContent='center' sx={{ m:2 }}>
+                <Typography variant="body1">{FileAbout.title === "" ? "タイトル未設定" : FileAbout.title }</Typography>
+                <Typography variant="body1">コメント　：　{FileAbout.comment === "" ? "コメント未設定" : FileAbout.comment }</Typography>
+                <Typography variant="body1">ファイル名：　{FileAbout.fileName === "" ? "" : FileAbout.fileName }</Typography>
+                <Typography variant="body1">パスワード：　{FileAbout.key === "0000" ? "パスワードが設定されていません" : FileAbout.key }</Typography>
+                <Typography variant="body1">有効期限　：　{FileAbout.limit}</Typography>
+                <TextField value={FileAbout.url} variant="standard" sx={{ px:2 }}/>
             </Stack>
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        </MainLayout>
+        </InfoLayout>
     )
 }
 
--- front/nkc-cloud/src/router/pageRouter.tsx
+++ front/nkc-cloud/src/router/pageRouter.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DownoladPage from "../components/pages/DownloadPage";
 import ErrorPage from "../components/pages/ErrorPage";
 import HomePage from "../components/pages/HomePage";
 import SendPage from "../components/pages/SendPage";
@@ -9,6 +10,7 @@ const PageRouter:React.FC = () => {
             <Routes>
                 <Route path={"/"} element={<HomePage/>}/>
                 <Route path={"/SendPage"} element={<SendPage/>}/>
+                <Route path={"/DownloadPage/*"} element={<DownoladPage/>}/>
                 <Route path={"/*"} element={<ErrorPage/>}/>
             </Routes>
         </BrowserRouter>
