5b560a2c25c1f385f6d38d9152c407d7ddabaee6
yuzawa <busieasy98@gmail.com>
2023-02-24 16:34:54 +0900

    ルーティングを設定

--- back/nkc-cloud/config/routes.rb
+++ back/nkc-cloud/config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace 'api',format: 'json' do
+    namespace 'v1',format: 'json' do
+      resources :users  do
+
+        get 'index' , action: :userindex  
+
+        
+        collection do
+
+          get ':name/:password/login' , action: :login ,only: [:userindex,:create,:destroy,:update,:login]
+      end
+    end
+  end
+end
+
+
 end
