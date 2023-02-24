547418ec792b0744514cce7ac5528454e1bfcccc
yuzawa <busieasy98@gmail.com>
2023-02-24 19:10:42 +0900

    コンフリクト回避のため、Postのコードと統合

--- back/nkc-cloud/config/routes.rb
+++ back/nkc-cloud/config/routes.rb
@@ -1,24 +1,28 @@
-Rails.application.routes.draw do
- 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do   
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+    # Defines the root path route ("/")
+    # root "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  namespace 'api',format: 'json' do
-    namespace 'v1',format: 'json' do
-      resources :users  do
-
-        get 'index' , action: :userindex  
-
-        
-        collection do
-
-          get ':name/:password/login' , action: :login ,only: [:userindex,:create,:destroy,:update,:login]
+    namespace 'api',format: 'json' do
+      namespace 'v1',format: 'json' do
+        resources :users  do
+          get 'index' , action: :userindex  
+          collection do
+            get ':name/:password/login' , action: :login ,only: [:userindex,:create,:destroy,:update,:login]
+        end
       end
     end
   end
-end
 
-
-end
+    root 'posts#top'
+    get "https://aaa.com/:url" => "posts#top"
+    namespace 'api' do
+      namespace 'v1' do
+        resources :posts
+        get "posts/https://aaa.com/:url" => "posts#Confirm"
+        get "posts/https://aaa.com/:url/:key/download" => "posts#download"
+      end
+    end
+  end
+  
--- back/nkc-cloud/db/schema.rb
+++ back/nkc-cloud/db/schema.rb
@@ -11,13 +11,21 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_20_004727) do
-
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.string "comment"
+    t.date "date"
+    t.string "file_name"
+    t.string "key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
