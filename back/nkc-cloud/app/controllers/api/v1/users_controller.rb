3f3cbccab2a45f908cd2f13d08ecf16dc75fa482
yuzawa <busieasy98@gmail.com>
2023-03-03 16:19:09 +0900

    プルリク#22の要件に伴い、set_user関数を削除

--- back/nkc-cloud/app/controllers/api/v1/users_controller.rb
+++ back/nkc-cloud/app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
 
   # ユーザーごとの投稿一覧
   def userindex
-    @posts = Post.where(user_id: params[:user_id])
+    posts = Post.where(user_id: params[:user_id])
     user = User.find_by(id: params[:user_id])
-    
-    render json: { status: 'SUCCESS', message: 'Loaded the user', data: [@posts,user] }
-    
-    
+
+    render json: { status: 'SUCCESS', message: 'Loaded the user', data: [posts,user] }
   end
 
   def create
@@ -22,37 +19,34 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def destroy
-    @user.destroy
-    render json: { status: 'SUCCESS', message: 'Deleted the user', data: @user }
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: { status: 'SUCCESS', message: 'Deleted the user', data: user }
   end
 
   def update
-    if @user.update(user_params)
-      render json: { status: 'SUCCESS', message: 'Updated the user', data: @user }
+    user = User.find_by(id: params[:id])
+    if user.update(user_params)
+      render json: { status: 'SUCCESS', message: 'Updated the user', data: user }
     else
-      render json: { status: 'ERROR', message: 'failed update', data: @user.errors }
+      render json: { status: 'ERROR', message: 'failed update', data: user.errors }
     end
   end
 
   def login
-    @loginuser = User.find_by(name: params[:name],
+    loginuser = User.find_by(name: params[:name],
                               password: params[:password])
-
-    if @loginuser.present?
-      render json: { status: 'SUCCESS', message: 'logined', data: @loginuser }
+    # そのレコードが存在したら
+    if loginuser.present?
+      render json: { status: 'SUCCESS', message: 'logined' ,data: "true"}
     else
-      # 間違えた値を返却値として返す
-      @name = params[:name]
-      @password = params[:password]
-      render json: { status: 'ERROR', message: 'login error', data:["false" , @name , @password] }
+      render json: { status: 'ERROR', message: 'login error', data:"false" }
     end
   end
 
-  private
+   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  
 
   def user_params
     params.require(:user).permit(:name,:password)
